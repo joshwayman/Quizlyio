93628ae8140262474090cc5b24b676ad5f3db478
joshwayman <nowayman121@gmail.com>
2017-06-05 12:59:43 +1000

    Added the JS required for social shares

--- quizlyio.ec6.js
+++ quizlyio.ec6.js
@@ -236,3 +236,31 @@ var quizlyio;
 $(function(){ 
     quizlyio = new Quizlyio('quizlyio-container');
 });
+
+
+/*
+ * social_media_sharing.js
+ *
+ * Adapted from:
+ * https://jonsuh.com/blog/social-share-links/
+ * 
+ **/
+
+function windowPopup(url, width, height) {
+  // Calculate the position of the popup so
+  // it’s centered on the screen.
+  var left = (screen.width / 2) - (width / 2),
+      top = (screen.height / 2) - (height / 2);
+
+  window.open(
+    url,
+    "",
+    "menubar=no,toolbar=no,resizable=yes,scrollbars=yes,width=" + width + ",height=" + height + ",top=" + top + ",left=" + left
+  );
+}
+
+$(".js-social-share > a").on("click", function(e) {
+    e.preventDefault();
+
+    windowPopup($(this).attr("href"), 500, 300);
+});
